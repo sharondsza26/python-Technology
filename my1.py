9f04af91724b3c370696890ddb6683668d229236
sharondsza26 <sharondsouza26@gmail.com>
2020-03-13 11:46:03 +0530

    My first python programs

---
+++ my1.py
@@ -0,0 +1,17 @@
+i = 10
+print("The Value of i is",i)
+
+j = 20
+print("The Value of j is",j)
+
+a = i + j
+print("The Sum of i and j is",a)
+
+s = j - i
+print("The difference between i and j is", s)
+
+m = i * j
+print("The Product of i and j is",m)
+
+d = j / i
+print("The result of division of i and j is",d)
