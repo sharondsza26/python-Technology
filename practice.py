b39c59f820bf593f882fb6e95c1c362f91f2d4ff
sharondsza26 <sharondsouza26@gmail.com>
2020-03-19 13:34:35 +0530

    working of if satement

---
+++ practice.py
@@ -0,0 +1,14 @@
+i=3
+j=6
+if i>=3 and j<13 :
+    print('&&&')
+    j=31
+    if j <25:
+        print('@@@')
+elif j >20 :
+    if i <=3:
+        print('^^^')
+    else :
+        print('###')
+else :
+    print('$$$')
