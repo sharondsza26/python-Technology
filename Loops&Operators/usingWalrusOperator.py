47d50a6d2dab4131770b1de6f6c8ec2501e07375
sharondsza26 <sharondsouza26@gmail.com>
2020-07-05 18:44:02 +0530

    learning walrus operator in python

---
+++ Loops&Operators/usingWalrusOperator.py
@@ -0,0 +1,15 @@
+# Number Guess 
+import random
+print("Welcome, I am TakeChance game...Let's play")
+number = random.randint(1,10)
+print("I have already guessed a number between 1 and 10")
+print("You get 3 chances to get it right and win a prize")
+count = 0
+while((count := count + 1) < 3):              #Walrus Operator :=
+    guess = int(input("Please enter your Guess\n"))
+    if(guess == number):
+        print("You are lucky... you win the prize")
+        break
+else:
+    print("You exhuasted your turns. Better luck next time")
+print("See ya in the next game")
