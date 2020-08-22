2ad09b80faff17e8a3a16e1a3fe744cc98dd9dac
sharondsza26 <sharondsouza26@gmail.com>
2020-08-22 20:08:11 +0530

    Simple serial server

---
+++ Networking/SerialServer/serial-Echo/server.py
@@ -0,0 +1,20 @@
+import socket;
+
+print("Starting the EchoServer")
+ss = socket.socket()
+
+ss.bind(("localhost",8091))
+
+ss.listen(5);
+
+while True:
+  print("Waiting for client")
+  socket,add = ss.accept()
+  print("Connected to ",add)
+  data = "";
+  while data != "q":
+      data = socket.recv(1024).decode()
+      print("Client sent -> ",data);
+      socket.send(data.encode(encoding='utf_8',errors='strict'))
+  socket.close()
+ss.close()
