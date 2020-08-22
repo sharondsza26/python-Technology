08a817d72744bc13334d9c1d008fd3c656926ec2
sharondsza26 <sharondsouza26@gmail.com>
2020-08-22 19:06:02 +0530

    Creating object of class Tk and a main container

---
+++ Tkinter/stage1.py
@@ -0,0 +1,13 @@
+# Stage 1
+
+from tkinter import *
+
+root = Tk()         #creating obj of class Tk
+# ^ Main container
+
+root.mainloop()
+print("heloooo")
+print("heloooo")
+print("heloooo")
+# gets printed only after you kill the window
+# which means the thread comes back!
