cb24ae82ddb47c0134d35c811031789aea52b9cc
sharondsza26 <sharondsouza26@gmail.com>
2020-08-22 18:43:50 +0530

    Adding LapMonitor - with lapmonitor

---
+++ ConcurrentProgramming/ThreadSynch/4Conditional2.py
@@ -0,0 +1,89 @@
+# stage 4
+from threading import *
+
+import time
+
+
+
+lapMoniter = Condition()
+
+
+
+
+
+class TestBed:
+
+    def busyTracks(self):
+
+        with lapMoniter:
+            # calls acquire internally and after completion calls release
+
+            t = current_thread()
+
+            print("{} Entered Busy Tracks".format(t.name))
+
+            while(TestBed.lap < 5):
+
+                if(t.name == "Car"): print("brrrommsss")
+
+                else: print("vrrrommsss")
+
+                time.sleep(1)
+
+                TestBed.lap += 1
+
+                print("Lap = {}".format(TestBed.lap))
+
+            TestBed.lap = 0
+
+            print("{} Leaving Busy Tracks".format(t.name))
+
+
+
+TestBed.lap = 0
+
+
+
+class Bike(Thread):
+
+    def __init__(self):
+
+        Thread.__init__(self,name="Bike")
+
+
+
+    def run(self):
+
+        print("Bike Starts Journey")
+
+        TestBed().busyTracks()
+
+        print("Bike Ends Journey")
+
+
+
+
+
+class Car(Thread):
+
+    def __init__(self):
+
+        Thread.__init__(self,name="Car")
+
+
+
+    def run(self):
+
+        print("Car Starts Journey")
+
+        TestBed().busyTracks()
+
+        print("Car Ends Journey")
+
+
+
+Car().start()
+
+
+
+Bike().start()
